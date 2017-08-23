30aa84dd3013b98faa3822fa4a2c39eb02212e3d
demills <demills@ncsu.edu>
2017-08-23 11:00:52 -0400

    Added my @author tag in StudentDirectory.java.

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/directory/StudentDirectory.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/directory/StudentDirectory.java
@@ -13,6 +13,7 @@ import edu.ncsu.csc216.pack_scheduler.user.Student;
  * Maintains a directory of all students enrolled at NC State.
  * All students have a unique id.
  * @author Sarah Heckman
+ * @author Daniel Mills <demills>
  */
 public class StudentDirectory {
 	

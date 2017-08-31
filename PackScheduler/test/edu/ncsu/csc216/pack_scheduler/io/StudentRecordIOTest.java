3f9d1f7f75c9028d748518612a1cbb6a1fb5bc6b
demills <demills@ncsu.edu>
2017-08-30 22:33:52 -0400

    Added tests to StudentRecordIOTest.testReadStudentRecords():
    - StudentRecordIOTest.testWriteStudentRecords() still needs to be
    implemented.
    - A test of reading a file with duplicate Student entries needs to be
    added to StudentRecordsIOTest.testReadStudentRecords().
    - Comments need to be added for all of StudentRecordIOTest.

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/io/StudentRecordIOTest.java
@@ -10,65 +10,83 @@ import org.junit.Test;
 import edu.ncsu.csc216.pack_scheduler.user.Student;
 
 public class StudentRecordIOTest {
-	public static final String VALID_STUDENTS = "test-files/student_records.txt";
-	public static final String INVALID_STUDENTS = "test-files/invalid_student_records.txt";
-	public static final String VALID_STUDENTS_WITH_BLANK = "test-files/student_records_with_blank_line.txt";
-	public static final String ROOT_OWNED_FILE = "test-files/owned_by_root.txt";
+	public static final String INPUT_VALID_STUDENTS = "test-files/student_records.txt";
+	public static final String INPUT_INVALID_STUDENTS = "test-files/invalid_student_records.txt";
+	public static final String INPUT_VALID_STUDENTS_WITH_BLANK = "test-files/student_records_with_blank_line.txt";
+	public static final String EXPECTED_VALID_STUDENTS = "test-files/expected_full_student_records.txt";
+	public static final String EXPECTED_INVALID_STUDENTS = "test-files/expected_student_records.txt";
 
+	/**
+	 * Tests StudentRecordIO.readStudentRecords();
+	 */
 	@Test
-	public void testReadStudentRecord() {
-		ArrayList<Student> s = new ArrayList<Student>();
-		s = null;
+	public void testReadStudentRecords() {
+		ArrayList<Student> actual = new ArrayList<Student>();
+		actual = null;
 
 		// Tests passing an empty string filename.
 		try {
-			s = StudentRecordIO.readStudentRecords("");
+			actual = StudentRecordIO.readStudentRecords("");
 			fail();
 		} catch (FileNotFoundException e) {
-			assertNull(s);
+			assertNull(actual);
 		}
 
 		// Tests passing a null filename.
 		try {
-			s = StudentRecordIO.readStudentRecords(null);
+			actual = StudentRecordIO.readStudentRecords(null);
 			fail();
 		} catch (FileNotFoundException e) {
-			assertNull(s);
-		}
-
-		// Tests reading an inaccessible file.
-		try {
-			s = StudentRecordIO.readStudentRecords(ROOT_OWNED_FILE);
-			fail();
-		} catch (FileNotFoundException e) {
-			assertNull(s);
+			assertNull(actual);
 		}
 
 		// Tests reading a non-existent file.
 		try {
-			s = StudentRecordIO.readStudentRecords("non_existent_file.txt");
+			actual = StudentRecordIO.readStudentRecords("non_existent_file.txt");
 			fail();
 		} catch (FileNotFoundException e) {
-			assertNull(s);
+			assertNull(actual);
 		}
 
 		// Tests reading an empty line inserted between non-empty lines.
-		// The empty line should be skipped, but this allows the conditional
-		//   for "
+		// No exceptions should be thrown.
 		try {
-			ArrayList<Student> valid = StudentRecordIO.readStudentRecords(VALID_STUDENTS);
-			s = StudentRecordIO.readStudentRecords(VALID_STUDENTS_WITH_BLANK);
-			assertEquals(valid.size(), s.size());
-			assertEquals(s.get(0), valid.get(0));
-			assertEquals(s.get(s.size() - 1), valid.get(valid.size() - 1));
-		} catch (FileNotFoundException e) {
+			ArrayList<Student> expected = StudentRecordIO.readStudentRecords(EXPECTED_VALID_STUDENTS);
+			actual = StudentRecordIO.readStudentRecords(INPUT_VALID_STUDENTS_WITH_BLANK);
+
+			assertEquals(expected.size(), actual.size());
+			// Compares each Student in the two ArrayListactual. Since VALID_STUDENTS_WITH_BLANK
+			//   has the same records as VALID_STUDENTS, except for a single blank line 
+			//   inserted, the two should produce identical lists of Students.
+			for (int i = 0; i < expected.size(); i++) {
+				// assertEquals will use the overridden equals() method of 
+				//   Student, not of Object.
+				assertEquals(expected.get(i), actual.get(i));
+			}
+		} catch (Exception e) {
 			fail();
 		}
+		
+		// Tests reading a file with invalid entries, either duplicates or invalid 
+		//   Student entries. A record is invalid if StudentRecordIO.processStudent()
+		//   cannot read in valid tokens or a Student object cannot be created from 
+		//   validly read in tokens. The invalid entries should be omitted with
+		//   no exceptions thrown.
+		actual = null;
+		try {
+			actual = StudentRecordIO.readStudentRecords(INPUT_INVALID_STUDENTS);
+			assertNotNull(actual);
+			assertEquals(0, actual.size()); 
+		} catch (Exception e) {
+			fail();
+		}
+		
+		// TODO - add test that reads through duplicate entries in a file.
+		// Duplicates Student records have the same id.
 	}
 
 	@Test
-	public void testWriteStudentRecord() {
+	public void testWriteStudentRecords() {
 		// TODO
-		assertTrue(true);
 	}
 }

ad1603fc68febf5102dbdda9c42f65aad0973381
nnbenven <nnbenven@ncsu.edu>
2017-09-11 13:27:29 -0400

    Removed conditional that always evaluate to false in Student.hashCode()

--- PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
+++ PackScheduler/src/edu/ncsu/csc216/pack_scheduler/user/Student.java
@@ -235,12 +235,12 @@ public class Student {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((email == null) ? 0 : email.hashCode());
-		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + email.hashCode();
+		result = prime * result + firstName.hashCode();
+		result = prime * result + id.hashCode();
+		result = prime * result + lastName.hashCode();
 		result = prime * result + maxCredits;
-		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + password.hashCode();
 		return result;
 	}
 

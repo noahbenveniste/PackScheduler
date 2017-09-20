5dc99c41b855beb07641ca424a5add0fda31d7d7
nnbenven <nnbenven@ncsu.edu>
2017-09-20 16:33:51 -0400

    Added testCourseCatalog in CourseCatalogTest

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalogTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalogTest.java
@@ -3,10 +3,45 @@
  */
 package edu.ncsu.csc216.pack_scheduler.catalog;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 /**
- * @author Owner
+ * @author Noah Benveniste
+ * @author Daniel Mills
+ * @author Kevin Hildner
  *
  */
 public class CourseCatalogTest {
-
+	
+	/** Valid course records */
+	private final String validTestFile = "test-files/course_records.txt";
+	/** Course name */
+	private static final String NAME = "CSC216";
+	/** Course title */
+	private static final String TITLE = "Programming Concepts - Java";
+	/** Course section */
+	private static final String SECTION = "001";
+	/** Course credits */
+	private static final int CREDITS = 4;
+	/** Course instructor id */
+	private static final String INSTRUCTOR_ID = "sesmith5";
+	/** Course meeting days */
+	private static final String MEETING_DAYS = "MW";
+	/** Course start time */
+	private static final int START_TIME = 1330;
+	/** Course end time */
+	private static final int END_TIME = 1445;
+	
+	/**
+	 * Tests the construction of a course catalog 
+	 */
+	@Test
+	public void testCourseCatalog() {
+		//Test that the StudentDirectory is initialized to an empty list
+		CourseCatalog catalog = new CourseCatalog();
+		assertFalse(catalog.removeCourseFromCatalog(NAME, SECTION));
+		assertEquals(0, catalog.getCourseCatalog().length);
+	}
 }

1c7ced70edef7374c8542f3a586efee47f82fc5b
nnbenven <nnbenven@ncsu.edu>
2017-09-20 12:12:22 -0400

    Added methods to CourseCatalog
    - added the following methods: loadCourseFromFile,
    removeCourseFromCatalog, getCourseFromCatalog, getCourseCatalog

--- PackScheduler/src/catalog/CourseCatalog.java
+++ PackScheduler/src/catalog/CourseCatalog.java
@@ -3,13 +3,16 @@
  */
 package catalog;
 
-import java.util.ArrayList;
+import java.io.FileNotFoundException;
 
 import edu.ncsu.csc216.collections.list.SortedList;
 import edu.ncsu.csc216.pack_scheduler.course.Course;
+import edu.ncsu.csc216.pack_scheduler.io.CourseRecordIO;
+import edu.ncsu.csc216.pack_scheduler.course.ConflictException;
 
 /**
  * @author Kevin Hildner
+ * @author Noah Benveniste
  *
  */
 public class CourseCatalog {
@@ -27,5 +30,62 @@ public class CourseCatalog {
 	 */
 	public void newCourseCatalog() {
 		this.catalog = new SortedList<Course>();
+	}
+	
+	/**
+	 * 
+	 */
+	public void loadCoursesFromFile(String fileName) {
+		try {
+			catalog = CourseRecordIO.readCourseRecords(fileName);
+		} catch (FileNotFoundException e) {
+			throw new IllegalArgumentException("Unable to read file " + fileName);
+		}
+	}
+	
+	/**
+	 * 
+	 */
+	public boolean removeCourseFromCatalog(String name, String section) {
+		for (int i = 0; i < catalog.size(); i++) {
+			Course c = catalog.get(i);
+			if ((c.getName().equals(name)) && (c.getSection().equals(section))) {
+				catalog.remove(i);
+				return true;
+			}
+		}
+		//Return false if the course is not in the catalog
+		return false;
+	}
+	
+	/**
+	 * 
+	 */
+	public Course getCourseFromCatalog(String name, String section) {
+		for (int i = 0; i < catalog.size(); i++) {
+			Course c = catalog.get(i);
+			if ((c.getName().equals(name)) && (c.getSection().equals(section))) {
+				return c;
+			}
+		}
+		//Return null if the course is not in the catalog
+		return null;
+	}
+	
+	/**
+	 *
+	 */
+	public String[][] getCourseCatalog() {
+		//Initialize the string array
+		String[][] courseCatalog = new String[catalog.size()][3];
+		//Loop through the course catalog getting the required fields and adding them
+		//to the relevant cells in the string array
+		for (int i = 0; i < catalog.size(); i++) {
+			Course c = catalog.get(i);
+			courseCatalog[i][0] = c.getName();
+			courseCatalog[i][1] = c.getSection();
+			courseCatalog[i][2] = c.getTitle();
+		}
+		return courseCatalog;
 	}
 }

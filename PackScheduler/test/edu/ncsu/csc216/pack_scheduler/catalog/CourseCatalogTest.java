d756d6269b0496119bbde525f9026000bb515a5d
nnbenven <nnbenven@ncsu.edu>
2017-09-21 10:07:31 -0400

    Added new unit tests to CourseCatalogTest
    - Added method signatures for tests that need to be implemented

--- PackScheduler/test/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalogTest.java
+++ PackScheduler/test/edu/ncsu/csc216/pack_scheduler/catalog/CourseCatalogTest.java
@@ -17,6 +17,8 @@ public class CourseCatalogTest {
 	
 	/** Valid course records */
 	private final String validTestFile = "test-files/course_records.txt";
+	/** Invalid course records */
+	private final String invalidTestFile = "test-files/invalid_course_records.txt";
 	/** Course name */
 	private static final String NAME = "CSC216";
 	/** Course title */
@@ -43,5 +45,84 @@ public class CourseCatalogTest {
 		CourseCatalog catalog = new CourseCatalog();
 		assertFalse(catalog.removeCourseFromCatalog(NAME, SECTION));
 		assertEquals(0, catalog.getCourseCatalog().length);
+	}
+	
+	/**
+	 * Tests loading in a course catalog from a list of courses in a text file.
+	 */
+	@Test
+	public void testLoadCourseCatalog() {
+		//Test that invalid test files are ignored
+		CourseCatalog badCatalog = new CourseCatalog();
+		badCatalog.loadCoursesFromFile(invalidTestFile);
+		assertEquals(0, badCatalog.getCourseCatalog().length);
+		
+		//Load in a course catalog from a valid test file
+		CourseCatalog catalog = new CourseCatalog();
+		catalog.loadCoursesFromFile(validTestFile);
+		assertEquals(8, catalog.getCourseCatalog().length);
+		
+		//Test file that doesn't exist
+		try {
+			badCatalog.loadCoursesFromFile("Nonexistant File");
+			fail("Loaded file that doesn't exist");
+		} catch(IllegalArgumentException e) {
+			assertEquals(0, badCatalog.getCourseCatalog().length);
+		}
+	}
+	
+	/**
+	 * Tests that calling newCourseCatalog() on a populated CourseCatalog resets it to empty
+	 */
+	@Test
+	public void testNewCourseCatalog() {
+		//Load in a course catalog from a valid test file
+		CourseCatalog catalog = new CourseCatalog();
+		catalog.loadCoursesFromFile(validTestFile);
+		assertEquals(8, catalog.getCourseCatalog().length);
+		
+		//Check that calling the method resets the catalog
+		catalog.newCourseCatalog();
+		assertEquals(0, catalog.getCourseCatalog().length);
+	}
+	
+	/**
+	 * Tests adding a course to a catalog
+	 */
+	@Test
+	public void testAddCourseToCatalog() {
+		
+	}
+	
+	/**
+	 * Tests removing a course from a catalog
+	 */
+	@Test
+	public void testRemoveCourseFromCatalog() {
+		
+	}
+	
+	/**
+	 * Tests getting a course from a catalog
+	 */
+	@Test
+	public void testGetCourseFromCatalog() {
+		
+	}
+	
+	/**
+	 * Tests getting a course catalog
+	 */
+	@Test
+	public void testGetCourseCatalog() {
+		
+	}
+	
+	/**
+	 * Tests saving a course catalog
+	 */
+	@Test
+	public void testSaveCourseCatalog() {
+		
 	}
 }
